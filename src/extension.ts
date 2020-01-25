c3438e5700ebdb95149016922af4c84343a5bcc8
Cherkashin Alexander <cherkalexander@gmail.com>
2020-01-25 23:16:36 +0300

    Add command to use ToDo as a comit message

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,92 @@
-import * as vscode from 'vscode';
-import { TodoTreeNodeProvider } from './todoTree';
-import { TodoStore } from './todoStore';
-import { showCreateToDo, showNameBox, showDescriptionBox } from './todoInputs';
-import { TodoTreeItem } from './todoTreeItem';
-import { createStatusBarItem } from './statusBarItem';
+import * as vscode from "vscode";
+import { TodoTreeNodeProvider } from "./todoTree";
+import { TodoStore } from "./todoStore";
+import {
+  showCreateToDo,
+  showNameBox,
+  showDescriptionBox,
+  showToDoList
+} from "./todoInputs";
+import { TodoTreeItem } from "./todoTreeItem";
+import { createStatusBarItem } from "./statusBarItem";
+import { GitExtension } from "../typings/git";
 
 export function activate(context: vscode.ExtensionContext) {
-	const todoStore = new TodoStore(context);
+  const todoStore = new TodoStore(context);
 
-	const todoTreeNodeProvider = new TodoTreeNodeProvider(todoStore);
-	vscode.window.registerTreeDataProvider('todoListView', todoTreeNodeProvider);
+  const todoTreeNodeProvider = new TodoTreeNodeProvider(todoStore);
+  vscode.window.registerTreeDataProvider("todoListView", todoTreeNodeProvider);
 
-	const statusBarItem = createStatusBarItem(todoStore);
+  const statusBarItem = createStatusBarItem(todoStore);
 
-	context.subscriptions.push(
-		statusBarItem,
-		vscode.commands.registerCommand('todoList.addTodo', async () => {
-			const todo = await showCreateToDo();
+  context.subscriptions.push(
+    statusBarItem,
+    vscode.commands.registerCommand("todoList.addTodo", async () => {
+      const todo = await showCreateToDo();
 
-			todo && todoStore.addTodo(todo);
-		}),
-		vscode.commands.registerCommand('todoList.reopen', (item: TodoTreeItem) => {
-			todoStore.undoItem(<string>item.id);
-		}),
-		vscode.commands.registerCommand('todoList.complete', (item: TodoTreeItem) => {
-			todoStore.completeItem(<string>item.id);
-		}),
-		vscode.commands.registerCommand('todoList.editTitle', async (item: TodoTreeItem) => {
-			const todo = todoStore.getTodo(<string>item.id);
-			const newName = await showNameBox(todo?.title);
+      todo && todoStore.addTodo(todo);
+    }),
+    vscode.commands.registerCommand("todoList.reopen", (item: TodoTreeItem) => {
+      todoStore.undoItem(<string>item.id);
+    }),
+    vscode.commands.registerCommand(
+      "todoList.complete",
+      (item: TodoTreeItem) => {
+        todoStore.completeItem(<string>item.id);
+      }
+    ),
+    vscode.commands.registerCommand(
+      "todoList.editTitle",
+      async (item: TodoTreeItem) => {
+        const todo = todoStore.getTodo(<string>item.id);
+        const newName = await showNameBox(todo?.title);
 
-			newName && todoStore.updateTitle(<string>item.id, newName);
-		}),
-		vscode.commands.registerCommand('todoList.editDescription', async (item: TodoTreeItem) => {
-			const todo = todoStore.getTodo(<string>item.id);
-			const newDescription = await showDescriptionBox(todo?.description);
+        newName && todoStore.updateTitle(<string>item.id, newName);
+      }
+    ),
+    vscode.commands.registerCommand(
+      "todoList.editDescription",
+      async (item: TodoTreeItem) => {
+        const todo = todoStore.getTodo(<string>item.id);
+        const newDescription = await showDescriptionBox(todo?.description);
 
-			(newDescription != null) && todoStore.updateDescription(<string>item.id, newDescription);
-		}),
-		vscode.commands.registerCommand('todoList.delete', (item: TodoTreeItem) => {
-			todoStore.deleteTodo(<string>item.id);
-		}),
-	);
+        newDescription != null &&
+          todoStore.updateDescription(<string>item.id, newDescription);
+      }
+    ),
+    vscode.commands.registerCommand("todoList.delete", (item: TodoTreeItem) => {
+      todoStore.deleteTodo(<string>item.id);
+    }),
+    vscode.commands.registerCommand("todoList.useAsCommitMessage", async () => {
+      //TODO: to refactor it!
+      const gitExtension = vscode.extensions.getExtension<GitExtension>(
+        "vscode.git"
+      )?.exports;
+
+      if (gitExtension?.enabled) {
+        const repositories = gitExtension.getAPI(1).repositories;
+        if (repositories.length > 0) {
+          const todoTitle = await showToDoList(
+            todoStore.openedTodos.map(item => item.title)
+          );
+
+          if (todoTitle) {
+            repositories[0].inputBox.value = todoTitle;
+          }
+        } else {
+          vscode.window.showErrorMessage(
+            "No repositories found! Please, open a folder under git control."
+          );
+        }
+      } else {
+        //TODO: need to test it
+        vscode.window.showErrorMessage(
+          "Git Extension is disabled. Please, enable Git Extension before using this command."
+        );
+      }
+    })
+  );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
--- src/todoInputs.ts
+++ src/todoInputs.ts
@@ -1,42 +1,52 @@
-import * as vscode from 'vscode';
-import { ITodoItem, createTodoItem } from './todoItem';
+import * as vscode from "vscode";
+import { ITodoItem, createTodoItem } from "./todoItem";
 
 export async function showCreateToDo(): Promise<ITodoItem | null> {
-    const name = await showNameBox();
+  const name = await showNameBox();
 
-    if (name) {
-        const description = await showDescriptionBox();
+  if (name) {
+    const description = await showDescriptionBox();
 
+    return createTodoItem({
+      title: name,
+      description,
+      isCompleted: false
+    });
+  }
 
-        return createTodoItem({
-            title: name,
-            description,
-            isCompleted: false,
-        });
-    }
-
-    return null;
+  return null;
 }
 
 export async function showNameBox(value?: string): Promise<string | undefined> {
-    const name = await vscode.window.showInputBox({
-        prompt: 'Input TODO name',
-        placeHolder: 'Name',
-        value,
-        validateInput: text => {
-            return !text ? 'TODO name cannot be empty!' : null;
-        }
-    });
+  const name = await vscode.window.showInputBox({
+    prompt: "Input TODO name",
+    placeHolder: "Name",
+    value,
+    validateInput: text => {
+      return !text ? "TODO name cannot be empty!" : null;
+    }
+  });
 
-    return name;
+  return name;
 }
 
-export async function showDescriptionBox(value?: string): Promise<string | undefined> {
-    const description = await vscode.window.showInputBox({
-        prompt: 'Input TODO description',
-        placeHolder: 'Description (Optional)',
-        value,
-    });
+export async function showDescriptionBox(
+  value?: string
+): Promise<string | undefined> {
+  const description = await vscode.window.showInputBox({
+    prompt: "Input TODO description",
+    placeHolder: "Description (Optional)",
+    value
+  });
 
-    return description;
+  return description;
+}
+
+export async function showToDoList(
+  todos: string[]
+): Promise<string | undefined> {
+  return await vscode.window.showQuickPick(todos, {
+    canPickMany: false,
+    placeHolder: "Choose ToDo"
+  });
 }
